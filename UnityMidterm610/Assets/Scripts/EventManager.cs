706971dc8d613dabbf69e6cddee524d4c8489c54
Brad Snow <bradsnow94@hotmail.com>
2014-02-25 19:06:48 -0500

    First Commit

---
+++ UnityMidterm610/Assets/Scripts/EventManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+	public GameObject SphereOne;
+	public GameObject SphereTwo;
+	public GameObject SphereThree;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision Obj)
+	{
+		if (Obj.gameObject.tag == "CubeOne") {
+			Debug.Log("test");
+			SphereOne.renderer.material.color = Color.yellow;
+			SphereTwo.renderer.material.color = Color.yellow;
+			SphereThree.renderer.material.color = Color.yellow;
+				}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
